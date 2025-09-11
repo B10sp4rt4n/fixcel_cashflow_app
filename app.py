99a5e3235d13ec718c9d29ae9fab0edd9af43964
B10sp4rt4n <salvador_ruiz74@hotmail.com>
2025-09-11 11:42:34 -0600

    Organizar archivos en el paquete main y corregir importaciones

--- app.py
+++ app.py
@@ -7,12 +7,13 @@ import matplotlib.pyplot as plt
 from unidecode import unidecode
 
 # --- IMPORTACIONES CORREGIDAS ---
-from main import main_kpi
-from main import main_comparativo
-from main import heatmap_ventas
-from main import kpi_engine
-from main import schema_mapper # Usando el nombre corregido
-from main.kpi_engine import compute_kpi_cpc
+# Este es el contenido para el archivo main/__init__.py
+
+from . import main_kpi
+from . import main_comparativo
+from . import heatmap_ventas
+from . import kpi_engine
+from . import schema_maper 
 
 # 1) Subir archivo + elegir empresa (puede ser un selectbox o input)
 empresa = st.sidebar.text_input("Empresa / Perfil", value="default")
