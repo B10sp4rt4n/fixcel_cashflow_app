4f2a32f87cced6bd8ef63d7f2555768114722b06
B10sp4rt4n <salvador_ruiz74@hotmail.com>
2025-09-11 14:24:31 -0600

    rolback hmap 2

--- main/heatmap_ventas.py
+++ main/heatmap_ventas.py
@@ -11,12 +11,10 @@ from unidecode import unidecode
 from . import main_comparativo
 from . import kpi_engine
 from . import schema_maper 
-from . utils import normalizar_columnas
+from .utils import normalizar_columnas
 
 def run(df):   
 
-# ... aquí irían el resto de tus importaciones
-def run(df):
     st.title("📊 Heatmap de Ventas (Entrada Genérica)")
 
     mapa_columnas = {
